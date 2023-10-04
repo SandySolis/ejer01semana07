07380e3bae8ca0c3c71b2119363b0c4614cab19a
Sandy Solis <sandysolis0305@gmail.com>
2023-10-03 19:22:58 -0500

    Agregar archivos de proyecto.

---
+++ ejer01semana07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double suma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double termino = (16 - i) / (3.0 * i);
+                suma += termino;
+                Console.WriteLine("Resultado : " + suma);
+            }
+
+            Console.WriteLine(+suma);
+            Console.ReadKey();
+
+        }
+    }
+}
